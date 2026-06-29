fdb8f3ed854eda336a354b35acc84c27f2eca1ef
janhavi kolhe <rasika.it001@gmail.com>
2026-06-29 20:38:20 +0530

    Com_4

--- components/sections/Hero.jsx
+++ components/sections/Hero.jsx
@@ -33,7 +33,7 @@ const Hero = () => {
             {/* BUTTONS */}
             <div className="mt-8 flex flex-row items-center gap-3 flex-wrap">
 
-              <Link href="/article/jeevantika-lingalwar-redefining-access-in-the-age-of-ai">
+              <Link href="/article/george-patriki-turning-pain-into-purpose-in-the-fight-against-addiction">
                 <button className="bg-black text-white px-5 py-3 text-[13px] rounded-md hover:bg-black/80 transition">
                   Read full story →
                 </button>
